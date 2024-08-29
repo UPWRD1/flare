608858c9f411effd1d65b44b0ab029a3c3a7da5c
Luke Davis <96660898+UPWRD1@users.noreply.github.com>
2024-08-29 17:59:58 +0000

    reinstated typechecker

--- allegro/src/root/passes/midend/typechecking.rs
+++ allegro/src/root/passes/midend/typechecking.rs
@@ -1,178 +1,173 @@
-
-use crate::root::resource::ast::{self, Expr, Program, SymbolType};
-use moniker::{Binder, BoundPattern, BoundTerm, Embed, FreeVar, Scope, Var};
-use std::rc::Rc;
+use crate::root::resource::ast::{self, Program, SymbolTable, SymbolTableEntry, SymbolType};
 
 
 
-// #[derive(Debug, Clone)]
-// pub struct Typechecker {
-//     s: SymbolTable,
-//     t: TypeTable,
-// }
+#[derive(Debug, Clone)]
+pub struct Typechecker {
+    s: SymbolTable,
+}
 
-// impl Typechecker {
-//     pub fn new() -> Self {
-//         Typechecker {
-//             s: SymbolTable::new(),
-//             t: TypeTable::new(),
-//         }
-//     }
+impl Typechecker {
+    pub fn new() -> Self {
+        Typechecker {
+            s: SymbolTable::new(),
+        }
+    }
 
-//     pub fn check(&mut self, p: Program) {
-//         self.s.open_scope();
-//         for m in p.modules {
-//             for a in m.body {
-//                 match a {
-//                     crate::root::resource::ast::Ast::FnDef { name, body, args, rettype, .. } => {
-//                         let mut temp = vec![];
-//                         for a in args.clone() {
-//                             temp.push(a.1)
-//                         };
-//                         self.s.insert(SymbolTableEntry { rawname: name , kind: ast::SymbolTableEntryKind::Fn { args: temp, ret: rettype }});
-//                         self.s.open_scope();
-//                         for a in args {
-//                             self.s.insert(SymbolTableEntry { rawname: a.0, kind: ast::SymbolTableEntryKind::Variable(a.1) })
-//                         }
-//                         for e in body {
-//                             self.check_expr(e);
-//                         }
-//                         self.s.pop_scope();
-//                     },
+    pub fn check(&mut self, p: Program) {
+        self.s.open_scope();
+        for m in p.modules {
+            for a in m.body {
+                match a {
+                    crate::root::resource::ast::Ast::FnDef { name, body, args, rettype, .. } => {
+                        let mut temp = vec![];
+                        for a in args.clone() {
+                            temp.push(a.1)
+                        };
+                        self.s.insert(SymbolTableEntry { rawname: name , kind: ast::SymbolTableEntryKind::Fn { args: temp, ret: rettype }});
+                        self.s.open_scope();
+                        for a in args {
+                            self.s.insert(SymbolTableEntry { rawname: a.0, kind: ast::SymbolTableEntryKind::Variable(a.1) })
+                        }
+                        for e in body {
+                            self.check_expr(e);
+                        }
+                        self.s.pop_scope();
+                    },
 
-//                     _ => println!(""),
-//                 }
-//             }
-//         }
-//     }
+                    _ => println!(""),
+                }
+            }
+        }
+    }
 
-//     fn check_expr(&mut self, e: crate::root::resource::ast::Expr) -> ast::SymbolType {
-//         dbg!(self.clone());
-//         match e {
-//             crate::root::resource::ast::Expr::BinAdd { l, r } => {
-//                 let lt = self.check_expr(*l.clone());
-//                 let rt = self.check_expr(*r.clone());
-//                 if self.compare_ty(&lt,&rt) {
-//                     lt
-//                 } else {
-//                     panic!("Cannot add {lt:?} {l:?} with {rt:?} {r:?}")
-//                 }
-//             },
-//             crate::root::resource::ast::Expr::BinSub { l, r } => {
-//                 let lt = self.check_expr(*l.clone());
-//                 let rt = self.check_expr(*r.clone());
-//                 if self.compare_ty(&lt,&rt) {
-//                     lt
-//                 } else {
-//                     panic!("Cannot subtract {lt:?} {l:?} with {rt:?} {r:?}")
-//                 }
-//             },
-//             crate::root::resource::ast::Expr::BinMul { l, r } => {
-//                 let lt = self.check_expr(*l.clone());
-//                 let rt = self.check_expr(*r.clone());
-//                 if self.compare_ty(&lt,&rt) {
-//                     lt
-//                 } else {
-//                     panic!("Cannot multiply {lt:?} {l:?} with {rt:?} {r:?}")
-//                 }
-//             },
-//             crate::root::resource::ast::Expr::BinDiv { l, r } => {
-//                 let lt = self.check_expr(*l.clone());
-//                 let rt = self.check_expr(*r.clone());
-//                 if self.compare_ty(&lt,&rt) {
-//                     lt
-//                 } else {
-//                     panic!("Cannot divide {lt:?} {l:?} with {rt:?} {r:?}")
-//                 }
-//             },
-//             crate::root::resource::ast::Expr::Logical { l, op, r } => {
-//                 let lt = self.check_expr(*l.clone());
-//                 let rt = self.check_expr(*r.clone());
-//                 if self.compare_ty(&lt,&rt) {
-//                     lt
-//                 } else {
-//                     panic!("Cannot divide {lt:?} {l:?} with {rt:?} {r:?}")
-//                 }
-//             },            
-//             crate::root::resource::ast::Expr::Assignment { name, value } => {
-//                 let lt = name.get_symbol_name();
-//                 let valt = self.check_expr(*value.clone());
-//                 self.s.insert(SymbolTableEntry::new(lt, ast::SymbolTableEntryKind::Variable(valt.clone())));
-//                 return valt;
-//             },
-//             crate::root::resource::ast::Expr::MutableAssignment { name, value } =>  {
-//                 let lt = name.get_symbol_name();
-//                 let valt = self.check_expr(*value.clone());
-//                 self.s.insert(SymbolTableEntry::new(lt, ast::SymbolTableEntryKind::MutVariable(valt.clone())));
-//                 return valt;
-//             },
-//             crate::root::resource::ast::Expr::Closure { args, body } => todo!(),
-//             crate::root::resource::ast::Expr::Return { value } => {self.check_expr(*value)},
-//             crate::root::resource::ast::Expr::If { condition, then, otherwise } => todo!(),
-//             crate::root::resource::ast::Expr::Int(_) => ast::SymbolType::Int,
-//             crate::root::resource::ast::Expr::Flt(_) => ast::SymbolType::Flt,
-//             crate::root::resource::ast::Expr::Str(_) => ast::SymbolType::Str,
-//             crate::root::resource::ast::Expr::Bool(_) => ast::SymbolType::Bool,
-//             crate::root::resource::ast::Expr::Symbol(s) => {
-//                 self.s.get(s.clone()).expect(&format!("Could not find symbol {s}")).kind.get_t()
-//             },
+    fn check_expr(&mut self, e: crate::root::resource::ast::Expr) -> ast::SymbolType {
+        dbg!(self.clone());
+        match e {
+            crate::root::resource::ast::Expr::BinAdd { l, r } => {
+                let lt = self.check_expr(*l.clone());
+                let rt = self.check_expr(*r.clone());
+                if self.compare_ty(&lt,&rt) {
+                    lt
+                } else {
+                    panic!("Cannot add {lt:?} {l:?} with {rt:?} {r:?}")
+                }
+            },
+            crate::root::resource::ast::Expr::BinSub { l, r } => {
+                let lt = self.check_expr(*l.clone());
+                let rt = self.check_expr(*r.clone());
+                if self.compare_ty(&lt,&rt) {
+                    lt
+                } else {
+                    panic!("Cannot subtract {lt:?} {l:?} with {rt:?} {r:?}")
+                }
+            },
+            crate::root::resource::ast::Expr::BinMul { l, r } => {
+                let lt = self.check_expr(*l.clone());
+                let rt = self.check_expr(*r.clone());
+                if self.compare_ty(&lt,&rt) {
+                    lt
+                } else {
+                    panic!("Cannot multiply {lt:?} {l:?} with {rt:?} {r:?}")
+                }
+            },
+            crate::root::resource::ast::Expr::BinDiv { l, r } => {
+                let lt = self.check_expr(*l.clone());
+                let rt = self.check_expr(*r.clone());
+                if self.compare_ty(&lt,&rt) {
+                    lt
+                } else {
+                    panic!("Cannot divide {lt:?} {l:?} with {rt:?} {r:?}")
+                }
+            },
+            crate::root::resource::ast::Expr::Logical { l, op, r } => {
+                let lt = self.check_expr(*l.clone());
+                let rt = self.check_expr(*r.clone());
+                if self.compare_ty(&lt,&rt) {
+                    lt
+                } else {
+                    panic!("Cannot divide {lt:?} {l:?} with {rt:?} {r:?}")
+                }
+            },            
+            crate::root::resource::ast::Expr::Assignment { name, value } => {
+                let lt = name.get_symbol_name();
+                let valt = self.check_expr(*value.clone());
+                self.s.insert(SymbolTableEntry::new(lt, ast::SymbolTableEntryKind::Variable(valt.clone())));
+                return valt;
+            },
+            crate::root::resource::ast::Expr::MutableAssignment { name, value } =>  {
+                let lt = name.get_symbol_name();
+                let valt = self.check_expr(*value.clone());
+                self.s.insert(SymbolTableEntry::new(lt, ast::SymbolTableEntryKind::MutVariable(valt.clone())));
+                return valt;
+            },
+            crate::root::resource::ast::Expr::Closure { args, body } => todo!(),
+            crate::root::resource::ast::Expr::Return { value } => {self.check_expr(*value)},
+            crate::root::resource::ast::Expr::If { condition, then, otherwise } => todo!(),
+            crate::root::resource::ast::Expr::Int(_) => ast::SymbolType::Int,
+            crate::root::resource::ast::Expr::Flt(_) => ast::SymbolType::Flt,
+            crate::root::resource::ast::Expr::Str(_) => ast::SymbolType::Str,
+            crate::root::resource::ast::Expr::Bool(_) => ast::SymbolType::Bool,
+            crate::root::resource::ast::Expr::Symbol(s) => {
+                self.s.get(s.clone()).expect(&format!("Could not find symbol {s}")).kind.get_t()
+            },
             
-//             ast::Expr::FieldAccess(_) => todo!(),
-//             ast::Expr::Call { name, .. } => self.s.get(name.get_symbol_name()).unwrap().kind.get_t(),
-//             ast::Expr::Composition { l, r } => {
-//                 let lt = self.check_expr(*l.clone());
-//                 let rt = self.check_expr(*r.clone());
-//                 if self.compare_ty(&lt,&rt) {
-//                     lt
-//                 } else {
-//                     panic!("Cannot compose {lt:?} {l:?} with {rt:?} {r:?}")
-//                 }
-//             }            
-//         }
-//     }
+            ast::Expr::FieldAccess(_) => todo!(),
+            ast::Expr::Call { name, .. } => self.s.get(name.get_symbol_name()).unwrap().kind.get_t(),
+            ast::Expr::Composition { l, r } => {
+                let lt = self.check_expr(*l.clone());
+                let rt = self.check_expr(*r.clone());
+                if self.compare_ty(&lt,&rt) {
+                    lt
+                } else {
+                    panic!("Cannot compose {lt:?} {l:?} with {rt:?} {r:?}")
+                }
+            }            
+        }
+    }
 
-//     fn compare_ty(&mut self, l: &SymbolType, r: &SymbolType) -> bool {
-//         if l.is_generic() && !r.is_generic() {
-//             self.t.update(l.get_generic_name(), r.clone());
-//             return true
-//         }
-//         if r.is_generic() && !l.is_generic() {
-//             self.t.update(r.get_generic_name(), l.clone());
-//             return true
-//         }
-//         if r == l {
-//             true
-//         } else {
-//             false
-//         }
-//     }
-// }
+    fn compare_ty(&mut self, l: &SymbolType, r: &SymbolType) -> bool {
+        // if l.is_generic() && !r.is_generic() {
+        //     self.t.update(l.get_generic_name(), r.clone());
+        //     return true
+        // }
+        // if r.is_generic() && !l.is_generic() {
+        //     self.t.update(r.get_generic_name(), l.clone());
+        //     return true
+        // }
+        if r == l {
+            true
+        } else {
+            false
+        }
+    }
+}
 
-// #[derive(Debug, Clone)]
-// pub struct TypeTable {
-//     entries: Vec<TypeTableEntry>,
-// }
+#[derive(Debug, Clone)]
+pub struct TypeTable {
+    entries: Vec<TypeTableEntry>,
+}
 
-// #[derive(Debug, Clone)]
-// pub struct TypeTableEntry {
-//     name: String,
-//     value: SymbolType,
-// }
+#[derive(Debug, Clone)]
+pub struct TypeTableEntry {
+    name: String,
+    value: SymbolType,
+}
 
-// impl TypeTable {
-//     pub fn new() -> Self {
-//         Self { entries: vec![] }
-//     }
+impl TypeTable {
+    pub fn new() -> Self {
+        Self { entries: vec![] }
+    }
 
-//     pub fn insert(&mut self, name: String, value: SymbolType) {
-//         self.entries.push(TypeTableEntry { name, value })
-//     }
+    pub fn insert(&mut self, name: String, value: SymbolType) {
+        self.entries.push(TypeTableEntry { name, value })
+    }
 
-//     pub fn update(&mut self, name: String, value: SymbolType) {
-//         for mut i in &self.entries {
-//             if i.name == name {
-//                 i = &TypeTableEntry {name: name.clone(), value: value.clone()}
-//             }
-//         }
-//     }
-// }
+    pub fn update(&mut self, name: String, value: SymbolType) {
+        for mut i in &self.entries {
+            if i.name == name {
+                i = &TypeTableEntry {name: name.clone(), value: value.clone()}
+            }
+        }
+    }
+}
--- allegro/src/root/resource/ast.rs
+++ allegro/src/root/resource/ast.rs
@@ -1,4 +1,3 @@
-use moniker::BoundTerm;
 use std::rc::Rc;
 
 #[derive(Debug, Clone)]
@@ -39,7 +38,7 @@ pub struct FnArgLimit {
     pub limit: String,
 }
 
-#[derive(Debug, Clone,  Hash, Eq, PartialEq, Ord, PartialOrd, BoundTerm)]
+#[derive(Debug, Clone,  Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub enum LogicOp {
     CEQ,
     CLT,
@@ -48,7 +47,7 @@ pub enum LogicOp {
     CGE,
 }
 
-#[derive(Debug, Clone,  PartialEq, BoundTerm)]
+#[derive(Debug, Clone,  PartialEq)]
 pub enum Expr {
     BinAdd {
         l: Box<Expr>,
@@ -118,52 +117,52 @@ impl Expr {
 
 
 
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub struct SymbolTable {
-//     scopes: Vec<SymbolTableScope>,
-//     top: usize,
-// }
+#[derive(Debug, Clone)]
+pub struct SymbolTable {
+    scopes: Vec<SymbolTableScope>,
+    top: usize,
+}
 
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub struct SymbolTableScope {
-//     entries: Vec<SymbolTableEntry>
-// }
+#[derive(Debug, Clone)]
+pub struct SymbolTableScope {
+    entries: Vec<SymbolTableEntry>
+}
 
-// #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-// pub struct SymbolTableEntry {
-//     pub rawname: String,
-//     pub kind: SymbolTableEntryKind,
-// }
+#[derive(Debug, Clone, PartialEq)]
+pub struct SymbolTableEntry {
+    pub rawname: String,
+    pub kind: SymbolTableEntryKind,
+}
 
-// impl SymbolTableEntry {
-//     pub fn new(n: String, k: SymbolTableEntryKind) -> Self {
-//         Self {
-//             rawname: n,
-//             kind: k
-//         }
-//     }
-// }
-// #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-// pub enum SymbolTableEntryKind {
-//     Variable(SymbolType),
-//     MutVariable(SymbolType),
-//     Fn {
-//         args: Vec<SymbolType>,
-//         ret: SymbolType,
-//     }
-// }
+impl SymbolTableEntry {
+    pub fn new(n: String, k: SymbolTableEntryKind) -> Self {
+        Self {
+            rawname: n,
+            kind: k
+        }
+    }
+}
+#[derive(Debug, Clone, PartialEq)]
+pub enum SymbolTableEntryKind {
+    Variable(SymbolType),
+    MutVariable(SymbolType),
+    Fn {
+        args: Vec<SymbolType>,
+        ret: SymbolType,
+    }
+}
 
-// impl SymbolTableEntryKind {
-//     pub fn get_t(&self) -> SymbolType {
-//         match self {
-//             Self::Variable(a) => a.clone(),
-//             Self::MutVariable(a) => a.clone(),
-//             Self::Fn {ret, ..} => ret.clone(),
-//         }
-//     }
-// }
+impl SymbolTableEntryKind {
+    pub fn get_t(&self) -> SymbolType {
+        match self {
+            Self::Variable(a) => a.clone(),
+            Self::MutVariable(a) => a.clone(),
+            Self::Fn {ret, ..} => ret.clone(),
+        }
+    }
+}
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq, Ord, PartialOrd, BoundTerm)]
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub enum SymbolType {
     Int,
     Flt,
@@ -177,7 +176,7 @@ pub enum SymbolType {
     Struct(Vec<(String, SymbolType)>)
 }
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq, Ord, PartialOrd, BoundTerm)]
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub struct RcType {
     pub inner: Rc<SymbolType>,
 }
@@ -190,67 +189,67 @@ impl From<SymbolType> for RcType {
     }
 }
 
-// impl SymbolType {
-//     pub fn is_unknown(&self) -> bool {
-//         match self {
-//             SymbolType::Unknown => true,
-//             _ => false,
-//         }
-//     }
+impl SymbolType {
+    pub fn is_unknown(&self) -> bool {
+        match self {
+            SymbolType::Unknown => true,
+            _ => false,
+        }
+    }
 
-//     pub fn is_generic(&self) -> bool {
-//         match self {
-//             SymbolType::Generic(_) => true,
-//             _ => false,
-//         }
-//     }
+    pub fn is_generic(&self) -> bool {
+        match self {
+            SymbolType::Generic(_) => true,
+            _ => false,
+        }
+    }
 
-//     pub fn get_generic_name(&self) -> String {
-//         match self {
-//             SymbolType::Generic(n) => n.to_string(),
-//             _ => panic!("{self:?} is not a generic")
-//         }
-//     }
-// }
+    pub fn get_generic_name(&self) -> String {
+        match self {
+            SymbolType::Generic(n) => n.to_string(),
+            _ => panic!("{self:?} is not a generic")
+        }
+    }
+}
 
-// impl SymbolTable {
-//     pub fn new() -> Self {
-//         Self { scopes: vec![], top: 0 }
-//     }
+impl SymbolTable {
+    pub fn new() -> Self {
+        Self { scopes: vec![], top: 0 }
+    }
 
-//     pub fn open_scope(&mut self) {
-//         self.top += 1;
-//         self.scopes.push(SymbolTableScope { entries: vec![] })
-//     } 
+    pub fn open_scope(&mut self) {
+        self.top += 1;
+        self.scopes.push(SymbolTableScope { entries: vec![] })
+    } 
 
-//     pub fn pop_scope(&mut self) {
-//         self.top -= 1;
-//     }
+    pub fn pop_scope(&mut self) {
+        self.top -= 1;
+    }
 
-//     pub fn insert(&mut self, s: SymbolTableEntry) {
-//         //dbg!(self.clone());
-//         self.scopes[self.top - 1].entries.push(s);
-//     }
+    pub fn insert(&mut self, s: SymbolTableEntry) {
+        //dbg!(self.clone());
+        self.scopes[self.top - 1].entries.push(s);
+    }
 
-//     pub fn update(&mut self, s: SymbolTableEntry) {
-//         for i in self.top..0 {
-//             let cs = &self.scopes[i];
-//             for mut j in &cs.entries {
-//                 if j.rawname == s.rawname {
-//                     j = &s
-//                 }
-//             }
-//         }
-//     }
+    pub fn update(&mut self, s: SymbolTableEntry) {
+        for i in self.top..0 {
+            let cs = &self.scopes[i];
+            for mut j in &cs.entries {
+                if j.rawname == s.rawname {
+                    j = &s
+                }
+            }
+        }
+    }
 
-//     pub fn get(&mut self, n: String) -> Option<SymbolTableEntry> {
-//         for i in &self.scopes {
-//             for j in &i.entries {
-//                 if j.rawname == n {
-//                     return Some(j.clone())
-//                 }
-//             }
-//         }
-//         return None;
-//     }
-// }
+    pub fn get(&mut self, n: String) -> Option<SymbolTableEntry> {
+        for i in &self.scopes {
+            for j in &i.entries {
+                if j.rawname == n {
+                    return Some(j.clone())
+                }
+            }
+        }
+        return None;
+    }
+}
